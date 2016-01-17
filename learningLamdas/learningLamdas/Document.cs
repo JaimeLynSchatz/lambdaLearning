6d5416338df14e2c0b271e11ed15775f898f0175
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-17 13:06:10 -0800

    first commit

---
+++ learningLamdas/learningLamdas/Document.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningLambdas
+{
+    public class Document
+    {
+        // public delegate int FindResult(object obj1);
+        // Once the delegate is created, you can excapsulate anymember method that meets the signature and return type
+        // no method body needed
+        // decouple the class that declares the delegate from the class that uses the delegate
+
+        public string Text { get; set; }
+        public delegate int SendDoc();
+
+        public void ReportSendingResult(SendDoc sendingDelegate)
+        {
+            if (sendingDelegate() == 0)
+            { 
+                Console.WriteLine("Success");
+            }
+            else
+            { 
+                Console.WriteLine("Unable to send!");
+            }
+        }
+
+
+    }
+}
---
+++ learningLamdas/learningLamdas/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningLamdas
+{
+    public class EmailSender
+    {
+        private int sendResult;
+        public int SendEmail() // signature matches delegate, can be used
+        {
+            Console.WriteLine("Simulating sending email...");
+            sendResult = 0;
+            return sendResult;
+        }
+    }
+}
